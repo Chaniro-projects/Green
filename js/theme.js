f7978f3056091e4c79ded1a774d1ab944a45fd5a
Bastien BARET <bastien.baret@gmail.com>
2014-01-24 16:12:53 +0100

    first commit

---
+++ js/theme.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $(".checkbox input[type='checkbox']").change(function () {
+        if ($(this).is(':checked')) {
+            $(this).next().children(".icon").fadeToggle(150);
+        } else {
+            $(this).next().children(".icon").fadeToggle(150);
+        }
+    });
+
+    $(".radio").click(function () {
+        if ($(this).children("input").is(':checked')) {
+            $(".radio input[name='" + $(this).children("input").attr("name") + "']").prev().removeClass("radio_checked");
+            $(this).children("span:first-child").addClass("radio_checked");
+        }
+    });
+});
