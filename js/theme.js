8a77b8b17c916379b65f47a97dd1f340b729b3e8
Bastien BARET <bastien.baret@gmail.com>
2014-01-25 12:25:34 +0100

    login box, switch

--- js/theme.js
+++ js/theme.js
@@ -13,4 +13,40 @@
             $(this).children("span:first-child").addClass("radio_checked");
         }
     });
+
+    $(".navsearch").click(function () {
+        if (!$(this).hasClass("active")) {
+            $(this).addClass("active");
+            console.log("ok");
+        }
+    });
+
+    $(".navsearch input").focusout(function () {
+        if ($(this).val() == "")
+            $(this).parent().parent().removeClass("active");
+    });
+
+    $(".header .dropdown").click(function (e) {
+        $(this).toggleClass("active");
+        $(this).children("ul.menu").fadeToggle(300);
+    });
+
+    $(".switch").click(function (e) {
+        e.preventDefault();
+        $(this).toggleClass("active");
+        if ($(this).hasClass("active"))
+            $(this).children("input").prop('checked', true);
+        else
+            $(this).children("input").prop('checked', false);
+    });
+
+    $("[data-width]").bind("update", function () {
+        if ($(this).attr("data-width") == "100%")
+            $(this).css("width", "102%");
+        else {
+            $(this).css("width", $(this).attr("data-width"));
+        }
+        $(this).next().html($(this).attr("data-width"));
+    });
+    $("[data-width]").trigger("update");
 });
